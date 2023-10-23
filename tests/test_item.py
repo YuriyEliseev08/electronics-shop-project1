67897ba33bfa6441f790b6d5ae290216e50218c7
Yuriy Eliseev <136329249+YuriyEliseev08@users.noreply.github.com>
2023-10-23 23:59:48 +0700

    Тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+def test_init(item_test):
+    assert item_test.name == "test"
+    assert item_test.price == 10_000
+    assert item_test.quantity == 10
+
+def test_calculate_total_price(item_test):
+    assert item_test.calculate_total_price
+
+def test_apply_discount(item_test):
+    assert item_test.quantity
+
+
+
+
+
+
+
