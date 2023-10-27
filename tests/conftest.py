47837bee9c167226b318ea7b25fb59f3ee11be9d
Yuriy Eliseev <136329249+YuriyEliseev08@users.noreply.github.com>
2023-10-27 22:52:11 +0700

    new fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
 import pytest
 from src.item import Item
 
+
 @pytest.fixture
 def item_test():
-    return Item("test",10_000, 10)
+    return Item("test", 10_000, 10)
+
+
+@pytest.fixture
+def instantiate_from_csv():
+    Item.all.clear()
+    Item.instantiate_from_csv('d:\\tuting\\el_shop_yuriy\\src\\items.csv')
+    return Item.all
