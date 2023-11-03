9acded8c99255e0408e4284592240aa7368ee9fc
Yuriy Eliseev <136329249+YuriyEliseev08@users.noreply.github.com>
2023-11-03 22:13:33 +0700

    написаны тесты по заданию

--- tests/test_item.py
+++ tests/test_item.py
@@ -13,8 +13,20 @@ def test_calculate_total_price(item_test):
     assert item_test.calculate_total_price() == 100_000
 
 
-
 def test_apply_discount(item_test):
     item_test.pay_rate = 0.8
     item_test.apply_discount()
     assert item_test.price == 8_000
+
+
+def test_string_to_number(item_test):
+    assert item_test.string_to_number("5") == 5
+    assert item_test.string_to_number("5.0") == 5
+    assert item_test.string_to_number("5.5") == 5
+
+
+def test_repr(item_test):
+    assert item_test.__repr__() == "Item('test', 10000, 10)"
+
+def test_str(item_test):
+    assert item_test.__str__() == "test"
