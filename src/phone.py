6a8c70f849f18c8b87b465ed6f3bcf59c58bf75c
Yuriy Eliseev <136329249+YuriyEliseev08@users.noreply.github.com>
2023-11-10 22:35:21 +0700

    Написан код для 4 домашки

--- src/phone.py
+++ src/phone.py
@@ -2,26 +2,23 @@ from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self, name: str, prise: float, quantity: int, number_of_sim: int):
-        super().__init__(name, prise, quantity)
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        super().__init__(name, price, quantity)
         if number_of_sim > 0:
             self.__number_of_sim = number_of_sim
         else:
             raise ValueError("Количество физических SIM- карт должно быть целым числом и больше 0.")
 
     @property
-    def namber_of_sim(self):
+    def number_of_sim(self):
         return self.__number_of_sim
 
-    @namber_of_sim.setter
-    def namber_of_sim(self, sim_input):
+    @number_of_sim.setter
+    def number_of_sim(self, sim_input):
         if sim_input > 0:
             self.__number_of_sim = sim_input
         else:
             raise ValueError("Количество физических SIM- карт должно быть целым числом и больше 0.")
 
-    def __add__(self, other) -> int:
-        if isinstance(other, Item):
-            return self.quantity + other.quantity
-        else:
-            raise ValueError("Только предметы класса Item или Phone")
+    def __repr__(self):
+        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
